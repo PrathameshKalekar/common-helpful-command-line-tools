61fda44e527ed87c5a02076a7c52140129809215
Prathamesh Kalekar <kalekarprathamesh19@gmail.com>
2024-07-26 21:15:01 +0530

    Move command upgraded it can now use to rename folder also

--- cmd/rename.go
+++ cmd/rename.go
@@ -21,18 +21,16 @@ For example:
 pralex rename main.go rename.go    # renames main.go into rename.go `,
 	Args: cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		fileName := args[0]
-		renameName := args[1]
-		if _, err := os.Stat(fileName); err != nil {
-			fmt.Printf("The given file with filename %s does not exist or provide the file extention", fileName)
+		oldName := args[0]
+		newName := args[1]
+
+		if err := validateOldName(oldName); err != nil {
+			fmt.Println(err)
 			return
-		} else {
-			if strings.Contains(renameName, ".") {
-				os.Rename(fileName, renameName)
-				fmt.Printf("file %s is renamed to %s", fileName, renameName)
-				return
-			}
-			fmt.Printf("%s is not a valid file name provide proper name with extention", renameName)
+		}
+
+		if err := renameItem(oldName, newName); err != nil {
+			fmt.Println(err)
 			return
 		}
 	},
@@ -41,4 +39,30 @@ pralex rename main.go rename.go    # renames main.go into rename.go `,
 func init() {
 	rootCmd.AddCommand(renameCmd)
 
+}
+
+// validateOldName checks if the old file or folder exists
+func validateOldName(name string) error {
+	if _, err := os.Stat(name); os.IsNotExist(err) {
+		return fmt.Errorf("The given file or folder with name %s does not exist", name)
+	}
+	return nil
+}
+
+// renameItem renames a file or folder based on the new name
+func renameItem(oldName, newName string) error {
+	if strings.Contains(newName, ".") {
+		// Rename the file
+		if err := os.Rename(oldName, newName); err != nil {
+			return fmt.Errorf("Error renaming file %s to %s: %v", oldName, newName, err)
+		}
+		fmt.Printf("File %s is renamed to %s\n", oldName, newName)
+	} else {
+		// Rename the folder
+		if err := os.Rename(oldName, newName); err != nil {
+			return fmt.Errorf("Error renaming folder %s to %s: %v", oldName, newName, err)
+		}
+		fmt.Printf("Folder %s is renamed to %s\n", oldName, newName)
+	}
+	return nil
 }
